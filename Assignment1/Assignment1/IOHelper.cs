3a6b3ff3af9d2f6b6f600383a277dd95937a70bf
desairavi1101 <desai.ravi1101@gmail.com>
2016-09-10 17:53:40 +0530

    Committed Assignment 1

---
+++ Assignment1/Assignment1/IOHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment1
+{
+    class IOHelper
+    {
+
+        public static List<int>ReadFileToList(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath, false);
+            List<int> input = new List<int>();
+            string allValues = reader.ReadToEnd();
+            string []values = allValues.Split('\n');
+            foreach(string value in values)
+            {
+                input.Add(Int32.Parse(value));
+            }
+            return input;
+        }
+
+    }
+}
---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = IOHelper.ReadFileToList("input.txt").ToArray();
+            long inversions;
+            int []sortedArray = SortAndCountInversions(input, out inversions);
+
+            Console.WriteLine("Inversions: {0}",inversions);
+            //foreach(int i in sortedArray)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            Console.ReadKey();
+        }
+
+        static int[] SortAndCountInversions(int[] input, out long inversions)
+        {
+            if(input.Length == 1)
+            {
+                inversions = 0;
+                return input;
+            }
+            int mid = input.Length / 2;
+            long leftInversions, rightInversions, splitInversions;
+            int[] a = input.Take(mid).ToArray();
+            int[] b = input.Skip(mid).Take(input.Length - mid).ToArray();
+            a = SortAndCountInversions(a,out leftInversions);
+            b = SortAndCountInversions(b, out rightInversions);
+
+            int[] mergedArray = MergeAndCountSplitInversions(a, b, out splitInversions);
+            
+            inversions = leftInversions + rightInversions + splitInversions;
+            return mergedArray;
+        }
+        static int[] MergeAndCountSplitInversions(int[] a, int[] b, out long splitInversions)
+        {
+            int i = 0, j = 0;
+            splitInversions = 0;
+            List<int> mergedArray = new List<int>();
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    mergedArray.Add(a[i++]);
+
+                }
+                else
+                {
+                    splitInversions += a.Length - i;
+                    mergedArray.Add(b[j++]);
+                }
+            }
+            while (i < a.Length)
+            {
+                mergedArray.Add(a[i++]);
+            }
+            while (j < b.Length)
+            {
+                mergedArray.Add(b[j++]);
+            }
+            return mergedArray.ToArray();
+        }
+        
+    }
+}
